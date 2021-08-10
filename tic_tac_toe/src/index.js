7fdeab98f997c3259d06b7c1099336cb207e8878
Hansaem Kim <hansaem_kim@hotmail.com>
2021-08-10 16:13:51 -0400

    Finished Tic-Tac-Toe

--- tic_tac_toe/src/index.js
+++ tic_tac_toe/src/index.js
@@ -3,4 +3,7 @@ const Game = require("../ttt_node/game")// require appropriate file
 
 document.addEventListener("DOMContentLoaded", () => {
     const ttt = document.querySelector(".ttt");
+    const game = new Game();
+    const view = new View(game, ttt);
+
 });
--- tic_tac_toe/src/ttt-view.js
+++ tic_tac_toe/src/ttt-view.js
@@ -1,26 +1,51 @@
 class View {
   constructor(game, el) {
-      this.game = game;
-      this.el = el;
+      this.game = game,
+      this.el = el,
+      this.setupBoard(),
+      this.handleClick = this.handleClick.bind(this),
+      this.bindEvents()
   }
 
   setupBoard() {   
       const grid = document.createElement('ul');
-      for (let i = 0;i<3;i++){
+      for (let i=0;i<3;i++){
         for(let j=0; j<3;j++){
           const block = document.createElement('li');
           block.dataset.pos = JSON.stringify([i,j]);
           grid.append(block);
         }
       }
+      this.el.append(grid);
   }
   
-  bindEvents() {}
+  bindEvents() {
+    this.el.addEventListener("click", this.handleClick)
+  }
 
-  handleClick(e) {}
+  handleClick(e) {
+    const tar = e.target;
+    "LI" === tar.nodeName && this.makeMove(tar)
+  }
 
-  makeMove(square) {}
-
+  makeMove(square) {
+    const ele = JSON.parse(square.dataset.pos);
+    const player = this.game.currentPlayer;
+    try {
+        this.game.playMove(ele)
+    } catch (square) {
+        alert("This " + square.msg.toLowerCase())
+    }
+    square.classList.add(player);
+    
+    if (this.game.isOver()){
+      this.el.removeEventListener("click", this.handleClick);
+      const winner = this.game.winner();
+      this.el.classList.add("game-over");
+      const caption = document.createElement("figcaption");
+      caption.append(`You win, ${winner}`);
+      this.el.append(caption);
+    }
+  }
 }
-
 module.exports = View;
